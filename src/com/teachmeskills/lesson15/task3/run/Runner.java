ef5728946fa4880453fe8202f8e15decdd50b096
Alina-Ilkevich <alina.1qaz@gmail.com>
2024-03-03 12:48:29 +0300

    init commit

--- src/com/teachmeskills/lesson15/task3/run/Runner.java
+++ src/com/teachmeskills/lesson15/task3/run/Runner.java
@@ -1,9 +1,9 @@
 package com.teachmeskills.lesson15.task3.run;
 
-import com.teachmeskills.lesson15.task3.CollectionCreating;
+import com.teachmeskills.lesson15.task3.service.CollectionCreating;
 
 public class Runner {
     public static void main(String[] args) {
-        CollectionCreating.createCollection();
+        CollectionCreating.calculateArithmeticMean(CollectionCreating.createCollection());
     }
 }
---
+++ src/com/teachmeskills/lesson15/task3/service/CollectionCreating.java
@@ -0,0 +1,37 @@
+package com.teachmeskills.lesson15.task3.service;
+
+import com.teachmeskills.lesson15.task3.exception.WrongSizeException;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CollectionCreating {
+    public static ArrayList<Integer> createCollection(){
+        ArrayList<Integer> numbers = null;
+        try {
+            System.out.print("Enter the size of the collection: ");
+            Scanner scanner = new Scanner(System.in);
+            int size = scanner.nextInt();
+            numbers = new ArrayList<>(size);
+            for (int i = 0; i < size; i++){
+                int num = (int) (Math.random() * 100);
+                numbers.add(num);
+            }
+            System.out.println("Collection: " + numbers);
+        } catch (IllegalArgumentException e){
+            System.out.println("The collection size must be greater than 0");
+        }catch (InputMismatchException e) {
+            System.out.println("You need enter the number");
+        }
+        return numbers;
+    }
+    public static void calculateArithmeticMean(ArrayList<Integer> numbers){
+        double count = 0.0;
+        for (int i = 0; i < numbers.size(); i++){
+            count += numbers.get(i);
+        }
+        double arithmeticMean = count / numbers.size();
+        System.out.println("Arithmetic mean: " + arithmeticMean);
+    }
+}
